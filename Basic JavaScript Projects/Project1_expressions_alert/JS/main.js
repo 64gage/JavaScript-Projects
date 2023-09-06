916ca3963bd9f824fa87bbdfc28bdbc77277859e
64gage <138918934+64gage@users.noreply.github.com>
2023-09-06 05:47:57 -0500

    Commit Project1_expressions_alert folder to main
    Created Project1_expressions_alert folder
    Created index.html in said folder
    Created JavaScript_file.js in said folder
    Created folder JS in said folder
    Created file main.js in folder: JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+window.alert("Hello, World!"); ////alerting Hello, World!
+document.write("Hello, World! "); //writing Hello, World!
+var A = "Hello, World! "; //assigning variable Hello, World!
+document.write(A); //writing Hello, World!
+window.alert(A); //alerting Hello, World!
+document.write("\"Hello, world!\" " + "Hello, World! "); //writing "Hello, World!" twice 
+var B = "Hello," + " World! "; //assigning variable Hello, World!
+document.write(B); //writing Hello, World!
+var A = "Hi, World! ", B = "Hi, World! "; //assigning 2 variables "Hi, World!"
+document.write(A, B) //writing Hi, World! twice
+document.write(3 + 3) // writing an expression
