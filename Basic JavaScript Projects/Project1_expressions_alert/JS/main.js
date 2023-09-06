5683cc20353c826bd000e7dc5beecac3266e9f36
64gage <138918934+64gage@users.noreply.github.com>
2023-09-06 05:52:48 -0500

    Update main.js
    Created variables Sent1 & Sent2 and assigned them with string values
    Concatenated and printed Sent1 and Sent2

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -9,3 +9,5 @@ document.write(B); //writing Hello, World!
 var A = "Hi, World! ", B = "Hi, World! "; //assigning 2 variables "Hi, World!"
 document.write(A, B) //writing Hi, World! twice
 document.write(3 + 3) // writing an expression
+var Sent1 = " This is the begining of the string", Sent2 = " and this is the end of the string" //creates new variables and assigns new text
+document.write(Sent1 + Sent2) // concatenates Sent1 and Sent2
